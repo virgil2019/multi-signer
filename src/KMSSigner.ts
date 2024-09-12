d2a2ec425069eebeaf133b19f0f7e64afd5849cf
virgil2019 <danten0402@gmail.com>
2024-09-12 16:09:57 +0800

    fix: recover pk to get rc for KMS

--- src/KMSSigner.ts
+++ src/KMSSigner.ts
@@ -2,6 +2,7 @@ import { Signer } from './signer';
 import { KMS } from '@aws-sdk/client-kms';
 import { Sequence, fromBER } from 'asn1js';
 import { SignerType } from './types';
+import { ecrecover } from 'ethereumjs-util';
 
 export class KMSSigner implements Signer {
     kms: KMS;
@@ -50,14 +51,32 @@ export class KMSSigner implements Signer {
                 },
                 (err, data) => {
                     if (err) {
+                        console.error('err', err);
                         rej(err);
                     } else {
                         let signature = this.handleDecodedData(
                             Buffer.from(data!.Signature!)
                         );
+                        const r = signature.subarray(0, 32); // 32 字节的 r 值
+                        const s = signature.subarray(32, 64); // 32 字节的 s 值
+
+                        const pubKeyV0 = ecrecover(hash, 27, r, s);
+                        const pubKeyV1 = ecrecover(hash, 28, r, s);
+                        
+                        let rc = 0;
+                        if (this.pk.substring(2) == pubKeyV0.toString('hex')) {
+                            rc = 0;
+                        }
+                        else if (this.pk.substring(2) == pubKeyV0.toString('hex')) {
+                            rc = 1;
+                        }
+                        else {
+                            throw new Error("Public key can not be recorved");
+                        }
+
                         res({
-                            rc: signature.at(64)!,
-                            signature: signature.subarray(0, 64)
+                            rc,
+                            signature
                         });
                     }
                 }
