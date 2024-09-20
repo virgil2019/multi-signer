d7a745d9dc2c9fcba1da7ef8945936191fc572d7
virgil2019 <danten0402@gmail.com>
2024-09-20 16:46:34 +0800

    test: update test case

--- tests/kmsSigner.spec.ts
+++ tests/kmsSigner.spec.ts
@@ -4,6 +4,51 @@ import { SignerType } from "../src/types";
 const PK = '0x1befc9021b51ee768110d3b579fac51ceaf4192880b5f34409d9cfb2aa55649bd4443c3ca5762020d75d8f05202ac61d8230679ace5f10d7d37b51149e0d0d55';
 
 describe('KMS Signer', function () {
+    describe('construct', function () {
+        it('should pass', () => {
+            const signer = createSigner(SignerType.KMS, {
+                keyId: "KEYID",
+                pk: PK
+            });
+        });
+    });
+
+    describe('sign', function () {
+        it('should fail with the hash param not 32 bytes', async () => {
+            const signer = createSigner(SignerType.KMS, {
+                keyId: "KEYID",
+                pk: PK
+            });
+
+            try {
+                const message = Buffer.from('12345678');
+                expect(message.length).toBeLessThan(32);
+                await signer.sign(message);
+                throw 'It should throw';
+            } catch (e) {}
+
+            try {
+                const message = Buffer.from(
+                    '123456781234567812345678123456781234567812345678'
+                );
+                expect(message.length).toBeGreaterThan(32);
+                await signer.sign(message);
+                throw 'It should throw';
+            } catch (e) {}
+        });
+
+        it('should work with the message is 32 bytes', async () => {
+            const signer = createSigner(SignerType.KMS, {
+            keyId: "KEYID",
+            pk: PK
+        });
+            const message = Buffer.from('12345678123456781234567812345678');
+            expect(message.length).toBe(32);
+            const sig = await signer.sign(message);
+            expect(sig.signature.length).toBeGreaterThan(0);
+        });
+    });
+
     describe('Get public key', function () {
         const signer = createSigner(SignerType.KMS, {
             keyId: "KEYID",
